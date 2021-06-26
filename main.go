e58a22532dd852d078751331f0b6abd68eae5425
Choi Bong mun <choibongmun@Choiui-MacBook-Air.local>
2021-06-26 22:10:23 +0900

    go for 문

--- main.go
+++ main.go
@@ -6,13 +6,29 @@ import (
 )
 
 // naked return return 부분에 이미 명시함.
-func lenAndUpper(name string) (lenght int, uppercase string) {
-	lenght = len(name)
+func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done")
+	length = len(name)
 	uppercase = strings.ToUpper(name)
 	return
 }
 
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
+// func repeatMe(word ...string) {
+// 	fmt.Println(word)
+// }
+
 func main() {
-	totalLength, uppercase := lenAndUpper(" bong mun choi")
-	fmt.Println(totalLength, uppercase)
+	test := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(test)
+	//repeatMe("123", "dpjsadf", "alkjsdf", "test")
+	// totalLength, uppercase := lenAndUpper(" bong mun choi")
+	// fmt.Println(totalLength, uppercase)
 }
