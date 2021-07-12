e426443de087e67232129648a794903c7e48bd41
GameCenter01 <gamecenter01@GameCenter01ui-MacBookPro.local>
2021-07-12 22:52:58 +0900

    dictionary add

--- main.go
+++ main.go
@@ -8,13 +8,27 @@ import (
 
 func main() {
 
-	dictionary := mydict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("first")
-	// 간단한데 강력함.
+	dictionary := mydict.Dictionary{}
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
 	}
+
+	hello, _ := dictionary.Search(word)
+	fmt.Println("found : ", word, "definition : ", hello)
+
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	// definition, err := dictionary.Search("first")
+	// // 간단한데 강력함.
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println(definition)
+	// }
 
 }
