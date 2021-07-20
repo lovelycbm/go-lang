c7203bdbf59e57af8c0728bf4dc5b11330d786e7
GameCenter01 <gamecenter01@GameCenter01ui-MacBookPro.local>
2021-07-20 23:30:42 +0900

    go 공부

--- main.go
+++ main.go
@@ -9,20 +9,30 @@ import (
 func main() {
 
 	dictionary := mydict.Dictionary{}
-	word := "hello"
-	definition := "Greeting"
-	err := dictionary.Add(word, definition)
+	baseWord := "hello"
+
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+
+	word, err := dictionary.Search(baseWord)
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(word)
 	}
 
-	hello, _ := dictionary.Search(word)
-	fmt.Println("found : ", word, "definition : ", hello)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
-	err2 := dictionary.Add(word, definition)
-	if err2 != nil {
-		fmt.Println(err2)
-	}
+	// hello, _ := dictionary.Search(word)
+	// fmt.Println("found : ", word, "definition : ", hello)
+
+	// err2 := dictionary.Add(word, definition)
+	// if err2 != nil {
+	// 	fmt.Println(err2)
+	// }
 	// definition, err := dictionary.Search("first")
 	// // 간단한데 강력함.
 	// if err != nil {
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -5,8 +5,11 @@ import "errors"
 // Dictionary type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not found")
-var errWordExists = errors.New("word exists")
+var (
+	errNotFound   = errors.New("Not found")
+	errCantUpdate = errors.New("Cant update non-exsiting word")
+	errWordExists = errors.New("word exists")
+)
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -28,4 +31,20 @@ func (d Dictionary) Add(word string, def string) error {
 	}
 
 	return nil
+}
+
+func (d Dictionary) Update(word string, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = def
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
